ab61f51f58304bb35ecfbcd539597cf41906b9b5
PC1 <umdomby@yandex.ru>
2025-01-07 17:37:59 +0300

    edit

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { ProfileButton } from './profile-button';
 import { AuthModal } from './modals';
-import {DropmenuAdmin} from "@/components/dropmenu-admin";
 import {ModeToggle} from "@/components/buttonTheme";
 
 
@@ -24,7 +23,13 @@ export const Header: React.FC<Props> = ({  className }) => {
                 {/* Левая часть */}
                 <Link href="/">
                     <div className="flex items-center gap-4">
-                        <Image src="/logo.png" alt="Logo" width={65} height={65}/>
+                            <Image
+                                src="/logo.png"
+                                alt="Logo"
+                                width={65}
+                                height={65}
+                                priority
+                            />
                         <div>
                             <h1 className="text-2xl uppercase font-black">
                                 HEROES<span className="text-red-500">3</span>
@@ -39,9 +44,7 @@ export const Header: React.FC<Props> = ({  className }) => {
                 <div className="flex items-center gap-3">
                     <ModeToggle/>
                     <AuthModal open={openAuthModal} onClose={() => setOpenAuthModal(false)}/>
-
                     <ProfileButton onClickSignIn={() => setOpenAuthModal(true)}/>
-                    <DropmenuAdmin/>
                 </div>
             </Container>
         </header>
