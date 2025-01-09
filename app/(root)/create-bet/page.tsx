7ea97c83792869bc687c5d7c5fa1c0fc0c599e41
PC1 <umdomby@yandex.ru>
2025-01-09 21:06:07 +0300

    edit

--- app/(root)/create-bet/page.tsx
+++ app/(root)/create-bet/page.tsx
@@ -1,4 +1,3 @@
-
 import {prisma} from '@/prisma/prisma-client';
 import {getUserSession} from '@/components/lib/get-user-session';
 import {redirect} from 'next/navigation';
@@ -7,25 +6,39 @@ import {Container} from "@/components";
 import {Suspense} from "react";
 import Loading from "@/app/(root)/loading";
 
-export default async function AdminPage() {
-    const session = await getUserSession();
 
-    if (!session) {
-        return redirect('/not-auth');
+async function fetchData() {
+    try {
+        const session = await getUserSession();
+
+        if (!session) {
+            return redirect('/not-auth');
+        }
+
+        const [user, product, category, productItem] = await prisma.$transaction([
+            prisma.user.findFirst({where: {id: Number(session?.id)}}),
+            prisma.category.findMany(),
+            prisma.product.findMany(),
+            prisma.productItem.findMany(),
+        ]);
+        return {user, category, product, productItem};
+    } catch (e) {
+        console.error('Database Error:', e);
+        return {user: [], category: [], product: [], productItem: []}; // Return empty arrays on error
     }
+}
 
-    const user = await prisma.user.findFirst({where: {id: Number(session?.id)}});
-    const product = await prisma.product.findMany();
-    const category = await prisma.category.findMany();
-    const productItem = await prisma.productItem.findMany();
 
-    if (user) {
+export default async function AdminPage() {
+
+    const data = await fetchData()
+
+    if (data.user) {
         return (
-
-                <Suspense fallback={<Loading />}>
-                <CreateBet user={user} category={category} product={product} productItem={productItem}/>;
-                </Suspense>
-
+            <Suspense fallback={<Loading/>}>
+                <CreateBet user={data.user} category={data.category} product={data.product}
+                           productItem={data.productItem}/>;
+            </Suspense>
         );
     } else {
         return redirect('/not-auth');
