541e55f62b65d2399c0e2d0dd72a4ecde85a4d3c
PC1 <umdom2@gmail.com>
2025-01-19 11:00:47 +0300

    edit

--- app/app/(root)/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading(){
-    return <div style={{width: "50%", margin: "0 auto"}}> Loading...</div>
-}
--- app/components/lib/get-user-session.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/components/constants/auth-options';
-
-export const getUserSession = async () => {
-  const session = await getServerSession(authOptions);
-
-  return session?.user ?? null;
-};
