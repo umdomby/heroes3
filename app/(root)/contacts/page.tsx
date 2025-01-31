3184bbab618c510a6890d50d5ac8819605a56811
PC1 <umdom2@gmail.com>
2025-01-31 16:47:41 +0300

    edit

--- app/(root)/contacts/page.tsx
+++ app/(root)/contacts/page.tsx
@@ -9,42 +9,12 @@ import { BUY_PAY_POINTS } from "@/components/BUY_PAY_POINTS";
 import {CONTACTS} from "@/components/CONTACTS";
 
 export default async function Home() {
-    const session = await getUserSession();
 
-    if (!session) {
-        return redirect('/not-auth');
-    }
-
-    const user = await prisma.user.findFirst({ where: { id: Number(session?.id) } });
-
-    if (!user) {
-        return redirect('/not-auth');
-    }
-
-    // Получаем все закрытые ставки, в которых участвовал пользователь
-    const closedBets = await prisma.betCLOSED.findMany({
-        where: {
-            participantsCLOSED: {
-                some: {
-                    userId: user.id
-                }
-            }
-        },
-        include: {
-            participantsCLOSED: true, // Получаем всех участников, чтобы отобразить выигранные и проигранные ставки
-            player1: true,
-            player2: true,
-            creator: true,
-            category: true,
-            product: true,
-            productItem: true
-        }
-    });
 
     return (
         <Container className="w-[100%]">
             <Suspense fallback={<Loading />}>
-                <CONTACTS user={user} closedBets={closedBets} />
+                <CONTACTS/>
             </Suspense>
         </Container>
     );
