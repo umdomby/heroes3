ebb03039e9031297b254830e3efa32585b595d66
PC1 <umdom2@gmail.com>
2025-02-01 10:54:12 +0300

    edit

--- components/HEROES_CLIENT_CLOSED.tsx
+++ components/HEROES_CLIENT_CLOSED.tsx
@@ -20,13 +20,13 @@ interface BetParticipantCLOSED {
     amount: number;
     odds: number;
     profit: number;
-    player: string; // Assuming PlayerChoice is a string enum
+    player: string;
     isWinner: boolean;
     createdAt: Date;
     margin: number;
     isCovered: string;
     overlap: number;
-    return: number; // Add the return field
+    return: number;
 }
 
 interface BetCLOSED {
@@ -36,11 +36,11 @@ interface BetCLOSED {
     player2: { name: string };
     totalBetPlayer1: number;
     totalBetPlayer2: number;
-    oddsBetPlayer1: number; // Текущий коэффициент для игрока 1
-    oddsBetPlayer2: number; // Текущий коэффициент для игрока 2
+    oddsBetPlayer1: number;
+    oddsBetPlayer2: number;
     createdAt: Date;
-    margin: number | null; // Разрешаем margin быть null
-    winnerId: number | null; // ID победителя
+    margin: number | null;
+    winnerId: number | null;
 }
 
 interface Props {
@@ -58,13 +58,9 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
             </div>
 
             {closedBets.map((bet) => {
-                // Фильтруем ставки, в которых участвовал текущий пользователь
                 const userBets = bet.participantsCLOSED.filter((p: BetParticipantCLOSED) => p.userId === user.id);
 
-                // Если пользователь не участвовал в этой ставке, пропускаем её
                 if (userBets.length === 0) return null;
-
-                // const winnerName = bet.winnerId === bet.player1.name ? bet.player1.name : bet.player2.name;
 
                 return (
                     <div key={bet.id} className="border border-gray-700 mt-1">
@@ -74,27 +70,18 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                     <Table>
                                         <TableBody>
                                             <TableRow>
-                                                {/* Игрок 1 */}
-                                                <TableCell
-                                                    className="text-ellipsis overflow-hidden whitespace-nowrap w-[25%]">
+                                                <TableCell className="text-ellipsis overflow-hidden whitespace-nowrap w-[25%]">
                                                     <div>{bet.player1.name}</div>
                                                     <div>{Math.floor(bet.totalBetPlayer1 * 100) / 100}</div>
                                                 </TableCell>
-
-                                                {/* Игрок 2 */}
-                                                <TableCell
-                                                    className="text-ellipsis overflow-hidden whitespace-nowrap w-[25%]">
+                                                <TableCell className="text-ellipsis overflow-hidden whitespace-nowrap w-[25%]">
                                                     <div>{bet.player2.name}</div>
                                                     <div>{Math.floor(bet.totalBetPlayer2 * 100) / 100}</div>
                                                 </TableCell>
-
-                                                {/* Коэффициент для игрока 1 и 2 */}
                                                 <TableCell className="w-[15%]">
                                                     <div>{Math.floor(bet.oddsBetPlayer1 * 100) / 100}</div>
                                                     <div>{Math.floor(bet.oddsBetPlayer2 * 100) / 100}</div>
                                                 </TableCell>
-
-                                                {/* Прибыль/убыток */}
                                                 <TableCell className="text-ellipsis overflow-hidden whitespace-nowrap w-[40%]">
                                                     <div>
                                                         <span>{bet.player1.name}</span> :{' '}
@@ -102,15 +89,15 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                                             className={
                                                                 userBets
                                                                     .filter((p) => p.player === 'PLAYER1')
-                                                                    .reduce((sum, p) => sum + (p.isWinner ? p.profit : -p.amount), 0) >= 0
+                                                                    .reduce((sum, p) => sum + (p.isWinner ? p.profit : (p.return - p.amount)), 0) >= 0
                                                                     ? 'text-green-500'
                                                                     : 'text-red-500'
                                                             }
                                                         >
-            {Math.floor(userBets
-                .filter((p) => p.player === 'PLAYER1')
-                .reduce((sum, p) => sum + (p.isWinner ? p.profit : -p.amount), 0) * 100) / 100}
-        </span>
+                                                            {Math.floor(userBets
+                                                                .filter((p) => p.player === 'PLAYER1')
+                                                                .reduce((sum, p) => sum + (p.isWinner ? p.profit : (p.return - p.amount)), 0) * 100) / 100}
+                                                        </span>
                                                     </div>
                                                     <div>
                                                         <span>{bet.player2.name}</span> :{' '}
@@ -118,15 +105,15 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                                             className={
                                                                 userBets
                                                                     .filter((p) => p.player === 'PLAYER2')
-                                                                    .reduce((sum, p) => sum + (p.isWinner ? p.profit : -p.amount), 0) >= 0
+                                                                    .reduce((sum, p) => sum + (p.isWinner ? p.profit : (p.return - p.amount)), 0) >= 0
                                                                     ? 'text-green-500'
                                                                     : 'text-red-500'
                                                             }
                                                         >
-            {Math.floor(userBets
-                .filter((p) => p.player === 'PLAYER2')
-                .reduce((sum, p) => sum + (p.isWinner ? p.profit : -p.amount), 0) * 100) / 100}
-        </span>
+                                                            {Math.floor(userBets
+                                                                .filter((p) => p.player === 'PLAYER2')
+                                                                .reduce((sum, p) => sum + (p.isWinner ? p.profit : (p.return - p.amount)), 0) * 100) / 100}
+                                                        </span>
                                                     </div>
                                                 </TableCell>
                                             </TableRow>
@@ -137,7 +124,7 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                     <div className="m-1 p-4 rounded-lg">
                                         <h4 className="text-md font-semibold mb-2">Итоги матча:</h4>
                                         <p>
-                                            {/*Победитель: <strong>{winnerName}</strong>*/}
+                                            {/* Победитель: <strong>{winnerName}</strong> */}
                                         </p>
                                     </div>
 
@@ -145,7 +132,6 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                         <div className="m-1 p-4 rounded-lg">
                                             <h4 className="text-md font-semibold mb-2">Ваши ставки на этот матч:</h4>
                                             {userBets.map((participant) => {
-                                                // Calculate the overlap percentage
                                                 const profitToCover =
                                                     participant.amount * (participant.odds - 1);
                                                 const overlapPercentage =
@@ -174,23 +160,23 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                                         </p>
                                                         {participant.isCovered ? (
                                                             <p>
-    <span
-        className={
-            overlapPercentage === 0
-                ? 'text-purple-500'
-                : overlapPercentage === 100
-                    ? 'text-green-500'
-                    : 'text-yellow-500'
-        }
-    >
-        Ваша ставка была перекрыта на {Math.floor(participant.overlap * 100) / 100} Points (
-        {overlapPercentage}%)
-    </span>
+                                                                <span
+                                                                    className={
+                                                                        overlapPercentage === 0
+                                                                            ? 'text-purple-500'
+                                                                            : overlapPercentage === 100
+                                                                                ? 'text-green-500'
+                                                                                : 'text-yellow-500'
+                                                                    }
+                                                                >
+                                                                    Ваша ставка была перекрыта на {Math.floor(participant.overlap * 100) / 100} Points (
+                                                                    {overlapPercentage}%)
+                                                                </span>
                                                                 <br/>
                                                                 {participant.isWinner ? (
                                                                     <span className="text-green-500">
-            Прибыль: {Math.floor(participant.return * 100) / 100} Points
-        </span>
+                                                                        Прибыль: {Math.floor(participant.return * 100) / 100} Points
+                                                                    </span>
                                                                 ) : (
                                                                     <span
                                                                         className={
@@ -199,15 +185,15 @@ export const HEROES_CLIENT_CLOSED: React.FC<Props> = ({ user, closedBets }) => {
                                                                                 : 'text-red-500'
                                                                         }
                                                                     >
-            Потеря: {Math.floor((participant.return - participant.amount) * 100) / 100} Points
-        </span>
+                                                                        Потеря: {Math.floor((participant.return - participant.amount) * 100) / 100} Points
+                                                                    </span>
                                                                 )}
                                                             </p>
                                                         ) : (
                                                             <p>
-                    <span className="text-yellow-500">
-                        Ваша ставка не была перекрыта (0 Points, 0%)
-                    </span>
+                                                                <span className="text-yellow-500">
+                                                                    Ваша ставка не была перекрыта (0 Points, 0%)
+                                                                </span>
                                                                 <br/>
                                                             </p>
                                                         )}
