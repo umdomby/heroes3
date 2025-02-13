5f561094ad6636649e082de71ebac7ae5fc11fe6
PC1 <umdom2@gmail.com>
2025-02-13 11:01:21 +0300

    edit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,17 @@ import { HEROES_CLIENT } from "@/components/HEROES_CLIENT";
 import { getUserSession } from "@/components/lib/get-user-session";
 import { GlobalData } from "@/components/globalData";
 import TelegramNotification from '@/components/TelegramNotification';
-import {HEROES_CLIENT_NO_REG} from "@/components/HEROES_CLIENT_NO_REG";
+import { HEROES_CLIENT_NO_REG } from "@/components/HEROES_CLIENT_NO_REG";
 import BanedNotification from "@/components/BanedNotification";
+import Link from "next/link";
+
+const FixedLink = () => (
+    <div className="fixed bottom-4 right-4 p-4 shadow-lg rounded-lg z-50">
+        <p className="text-md text-blue-500 font-bold">
+            <Link className="text-blue-500 hover:text-green-300 font-bold text-xl" href={'https://t.me/navatar85'} target="_blank">@navatar85</Link>
+        </p>
+    </div>
+);
 
 export default async function Home() {
     const session = await getUserSession();
@@ -18,29 +27,36 @@ export default async function Home() {
         user = await prisma.user.findFirst({ where: { id: Number(session?.id) } });
     }
 
-    if (user && user.role !== 'BANED') {
-        const isTelegramEmpty = !user.telegram || user.telegram.trim() === '';
+    const isTelegramEmpty = user && (!user.telegram || user.telegram.trim() === '');
 
-        return (
-            <Container className="w-[100%]">
-                {isTelegramEmpty && (
-                    <TelegramNotification initialTelegram={user.telegram || ''} />
-                )}
-                <Suspense fallback={<Loading />}>
-                    <GlobalData />
-                    <HEROES_CLIENT user={user} />
-                </Suspense>
-            </Container>
-        );
-    } else {
-        return (
-            <Container className="w-[100%]">
+    return (
+        <Container className="w-[100%]">
+            {user && user.role !== 'BANED' && (
+                <>
+                    {isTelegramEmpty && (
+                        <TelegramNotification initialTelegram={user.telegram || ''} />
+                    )}
+                    <FixedLink />
+                    <Suspense fallback={<Loading />}>
+                        <GlobalData />
+                        <HEROES_CLIENT user={user} />
+                    </Suspense>
+                </>
+            )}
+            {user && user.role === 'BANED' && (
                 <Suspense fallback={<Loading />}>
                     <BanedNotification />
                     <GlobalData />
                     <HEROES_CLIENT_NO_REG />
                 </Suspense>
-            </Container>
-        );
-    }
+            )}
+            {!user && (
+                <Suspense fallback={<Loading />}>
+                    <FixedLink />
+                    <GlobalData />
+                    <HEROES_CLIENT_NO_REG />
+                </Suspense>
+            )}
+        </Container>
+    );
 }
