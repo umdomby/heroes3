a3a13328822cc729cc751ab27b167b3bd68538e2
PC1 <umdom2@gmail.com>
2025-02-18 18:01:04 +0300

    edit

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -25,161 +25,202 @@ export const players = [
     {
         "id": 1,
         "name": "pup_ok_",
+        "twitch": 'https://www.twitch.tv/pup_ok_',
     },
     {
         "id": 2,
         "name": "HotaGirl",
+        "twitch": 'https://www.twitch.tv/HotaGirl',
     },
     {
         "id": 3,
         "name": "2BISHOP",
+        "twitch": 'https://www.twitch.tv/2BISHOP',
     },
     {
         "id": 4,
         "name": "HILTYHA",
+        "twitch": 'https://www.twitch.tv/HILTYHA',
     },
     {
         "id": 5,
         "name": "Master_of__mind",
+        "twitch": 'https://www.twitch.tv/Master_of__mind',
     },
     {
         "id": 6,
         "name": "yarostnayaKoshka",
+        "twitch": 'https://www.twitch.tv/yarostnayaKoshka',
     },
     {
         "id": 7,
         "name": "kesZu_h3",
+        "twitch": 'https://www.twitch.tv/kesZu_h3',
     },
     {
         "id": 8,
         "name": "KnyazeMakarSumski",
+        "twitch": 'https://www.twitch.tv/KnyazeMakarSumski',
     },
     {
         "id": 9,
         "name": "KICK_FREAK",
+        "twitch": 'https://www.twitch.tv/KICK_FREAK',
     },
     {
         "id": 10,
         "name": "Redwhait",
+        "twitch": 'https://www.twitch.tv/Redwhait',
     },
     {
         "id": 11,
         "name": "ViSoVi",
+        "twitch": 'https://www.twitch.tv/ViSoVi',
     },
     {
         "id": 12,
         "name": "sstrattegy",
+        "twitch": 'https://www.twitch.tv/sstrattegy',
     },
     {
         "id": 13,
         "name": "ARTI_da_KING",
+        "twitch": 'https://www.twitch.tv/ARTI_da_KING',
     },
     {
         "id": 14,
         "name": "YAR_",
+        "twitch": 'https://www.twitch.tv/YAR_',
     },
     {
         "id": 15,
         "name": "newb1kk",
+        "twitch": 'https://www.twitch.tv/newb1kk',
     },
     {
         "id": 16,
         "name": "tyranuxus11",
+        "twitch": 'https://www.twitch.tv/tyranuxus11',
     },
     {
         "id": 17,
         "name": "Evlampich",
+        "twitch": 'https://www.twitch.tv/Evlampich',
     },
     {
         "id": 18,
         "name": "Pavllovich",
+        "twitch": 'https://www.twitch.tv/Pavllovich',
     },
     {
         "id": 19,
         "name": "Sav1tarrr",
+        "twitch": 'https://www.twitch.tv/Sav1tarrr',
     },
     {
         "id": 20,
         "name": "papashkaiz4atika",
+        "twitch": 'https://www.twitch.tv/papashkaiz4atika',
     },
     {
         "id": 21,
         "name": "Tender_cat",
+        "twitch": 'https://www.twitch.tv/Tender_cat',
     },
     {
         "id": 22,
         "name": "Relig00s",
+        "twitch": 'https://www.twitch.tv/Relig00s',
     },
     {
         "id": 23,
         "name": "amieloo",
+        "twitch": 'https://www.twitch.tv/amieloo',
     },
     {
         "id": 24,
         "name": "M_on_t",
+        "twitch": 'https://www.twitch.tv/M_on_t',
     },
     {
         "id": 25,
         "name": "RitoSux",
+        "twitch": 'https://www.twitch.tv/RitoSux',
     },
     {
         "id": 26,
         "name": "beZZdar_",
+        "twitch": 'https://www.twitch.tv/beZZdar_',
     },
     {
         "id": 27,
         "name": "AnzhPri",
+        "twitch": 'https://www.twitch.tv/AnzhPri',
     },
     {
         "id": 28,
         "name": "MeatWagonGG",
+        "twitch": 'https://www.twitch.tv/MeatWagonGG',
     },
     {
         "id": 29,
         "name": "mrplane_",
+        "twitch": 'https://www.twitch.tv/mrplane_',
     },
     {
         "id": 30,
         "name": "ChambiQ",
+        "twitch": 'https://www.twitch.tv/ChambiQ',
     },
     {
         "id": 31,
         "name": "akaStinger",
+        "twitch": 'https://www.twitch.tv/akaStinger',
     },
     {
         "id": 32,
         "name": "Tihiy__",
+        "twitch": 'https://www.twitch.tv/Tihiy__',
     },
     {
         "id": 33,
         "name": "unutcon",
+        "twitch": 'https://www.twitch.tv/unutcon',
+
     },
     {
         "id": 34,
         "name": "Daddy_Boka",
+        "twitch": 'https://www.twitch.tv/Daddy_Boka',
     },
     {
         "id": 35,
         "name": "GomungulsTV",
+        "twitch": 'https://www.twitch.tv/GomungulsTV',
     },
     {
         "id": 36,
         "name": "HellLighT111",
+        "twitch": 'https://www.twitch.tv/HellLighT111',
     },
     {
         "id": 37,
         "name": "Weronest",
+        "twitch": 'https://www.twitch.tv/Weronest',
     },
     {
         "id": 38,
         "name": "VovastikMashina",
+        "twitch": 'https://www.twitch.tv/VovastikMashina',
     },
     {
         "id": 39,
         "name": "zherarrr",
+        "twitch": 'https://www.twitch.tv/zherarrr',
     },
     {
         "id": 40,
         "name": "Wukosha",
+        "twitch": 'https://www.twitch.tv/Wukosha',
     },
 ];
