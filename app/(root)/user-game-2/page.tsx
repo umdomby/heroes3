daae483c18422a8d09241965d744bfcf492d4f3d
PC1 <umdom2@gmail.com>
2025-02-22 18:19:58 +0300

    edit

--- app/(root)/user-game-2/page.tsx
+++ app/(root)/user-game-2/page.tsx
@@ -4,8 +4,8 @@ import { prisma } from '@/prisma/prisma-client';
 import React from "react";
 import { getUserSession } from "@/components/lib/get-user-session";
 import { UserGame2Comp } from "@/components/user-game-2-comp";
-import {Container} from '@/components/container';
-import {redirect} from "next/navigation";
+import { Container } from '@/components/container';
+import { redirect } from "next/navigation";
 import Link from "next/link";
 
 export default async function UserGame2Page() {
@@ -16,29 +16,29 @@ export default async function UserGame2Page() {
     }
 
     const user = await prisma.user.findFirst({ where: { id: Number(session?.id) } });
-    const player = prisma.player.findFirst({ where: { userId: Number(session?.id) }});
-
-    console.log(player)
 
     if (!user) {
         return redirect('/not-auth');
     }
+    const player = await prisma.player.findFirst({ where: { userId: Number(session?.id) } });
 
-    if (!player.userId) {
-        return <div className="text-center">
-            <p className="text-green-500">Вы не зарегистрированы как игрок</p>
-            <p>
-                1. Заполните: Настройки Telegram
-            </p>
-            <p>
-                2. Зарегистрируйтесь как игрок
-            </p>
-            <p>
-                <Link href="/profile" className="text-blue-500">Profile</Link>
-            </p>
-        </div>;
+    // Проверяем, существует ли объект player
+    if (!player) {
+        return (
+            <div className="text-center">
+                <p className="text-green-500">Вы не зарегистрированы как игрок</p>
+                <p>
+                    1. Заполните: Настройки Telegram
+                </p>
+                <p>
+                    2. Зарегистрируйтесь как игрок
+                </p>
+                <p>
+                    <Link href="/profile" className="text-blue-500">Profile</Link>
+                </p>
+            </div>
+        );
     }
-
 
     return (
         <Container className="flex flex-col my-10 w-[96%]">
