d670ce06c08f0166e9944e4150700169b730df90
PC1 <umdom2@gmail.com>
2025-02-28 01:38:31 +0300

    edit

--- app/(root)/order-p2p/page.tsx
+++ app/(root)/order-p2p/page.tsx
@@ -55,7 +55,7 @@ export default async function OrderP2PPage() {
     //await updateCurrencyRatesIfNeeded();
 
     // Закрываем просроченные сделки перед рендерингом страницы
-    // await checkAndCloseOrderP2PTime();
+    await checkAndCloseOrderP2PTime();
 
     const openOrders = await prisma.orderP2P.findMany({
         where: { orderP2PStatus: 'OPEN' },
