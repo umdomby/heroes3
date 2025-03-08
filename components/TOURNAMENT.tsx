32c5abc04f6acd92133e843a86ac52f684e27add
PC1 <umdom2@gmail.com>
2025-03-08 13:41:56 +0300

    edit

--- components/TOURNAMENT.tsx
+++ components/TOURNAMENT.tsx
@@ -43,7 +43,7 @@ interface PlayerStatisticsProps {
     playerStatistics: PlayerStatisticWithRelations[];
     currentPage: number;
     totalPages: number;
-    user: User;
+    user: User | null;
     turnirs: { id: number; name: string }[];
     categories: { id: number; name: string }[];
     players: { id: number; name: string }[];
@@ -71,6 +71,8 @@ export function TOURNAMENT({
         win: false,
         link: ''
     });
+    // Проверка на заполненность всех полей
+    const isFormValid = Object.values(formData).every(value => value !== '' && value !== 0);
 
     const handlePageChange = (newPage: number) => {
         router.push(`?page=${newPage}`);
@@ -89,7 +91,8 @@ export function TOURNAMENT({
     };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value, type, checked } = e.target;
+        const { name, value, type } = e.target;
+        const checked = (e.target as HTMLInputElement).checked; // Приведение типа для checkbox
         setFormData({
             ...formData,
             [name]: type === 'checkbox' ? checked : value
@@ -110,108 +113,6 @@ export function TOURNAMENT({
 
     return (
         <div>
-            {(user?.role === 'ADMIN' || user?.role === 'USER_EDIT') && (
-                <div>
-                    <form onSubmit={handleSubmit} className="mb-6">
-                        <div className="flex flex-wrap gap-4">
-                            <Select onValueChange={(value) => setFormData({ ...formData, turnirId: value })}>
-                                <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Выберите турнир" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {turnirs.map((turnir) => (
-                                        <SelectItem key={turnir.id} value={turnir.id.toString()}>
-                                            {turnir.name}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <Select onValueChange={(value) => setFormData({ ...formData, categoryId: value })}>
-                                <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Выберите категорию" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {categories.map((category) => (
-                                        <SelectItem key={category.id} value={category.id.toString()}>
-                                            {category.name}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <Select onValueChange={(value) => setFormData({ ...formData, playerId: value })}>
-                                <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Выберите игрока" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {players.map((player) => (
-                                        <SelectItem key={player.id} value={player.id.toString()}>
-                                            {player.name}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <Select onValueChange={(value) => setFormData({ ...formData, color: value })}>
-                                <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Выберите цвет" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {Object.keys(ColorPlayer).map((color) => (
-                                        <SelectItem key={color} value={color}>
-                                            {ColorPlayer[color as keyof typeof ColorPlayer]}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <Select onValueChange={(value) => setFormData({ ...formData, city: value })}>
-                                <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Выберите город" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {Object.keys(cityTranslations).map((city) => (
-                                        <SelectItem key={city} value={city}>
-                                            {cityTranslations[city]}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <Input
-                                type="number"
-                                name="gold"
-                                value={formData.gold}
-                                onChange={handleInputChange}
-                                placeholder="Gold"
-                            />
-                            <Input
-                                type="text"
-                                name="security"
-                                value={formData.security}
-                                onChange={handleInputChange}
-                                placeholder="Security"
-                            />
-                            <Input
-                                type="checkbox"
-                                name="win"
-                                checked={formData.win}
-                                onChange={handleInputChange}
-                            />
-                            <Input
-                                type="text"
-                                name="link"
-                                value={formData.link}
-                                onChange={handleInputChange}
-                                placeholder="Link"
-                            />
-                            <Button type="submit" className="h-7">
-                                Сохранить
-                            </Button>
-                            <Button className="h-7" onClick={handleCalculateStatistics} disabled={isPending}>
-                                {isPending ? 'Обновление...' : 'Обновить статистику'}
-                            </Button>
-                        </div>
-                    </form>
-                </div>
-            )}
-
             <div className="pagination-buttons flex justify-center items-center m-6">
                 <div className="flex justify-center w-full">
                     <Button
@@ -307,6 +208,108 @@ export function TOURNAMENT({
                     Следующая
                 </Button>
             </div>
+            {(user?.role === 'ADMIN' || user?.role === 'USER_EDIT') && (
+                <div>
+                    <div className="m-4 text-center text-amber-500 text-xl">Создать запись</div>
+                    <form onSubmit={handleSubmit} className="mb-6">
+                        <div className="flex flex-wrap gap-4">
+                            <Select onValueChange={(value) => setFormData({...formData, turnirId: value})}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Выберите турнир"/>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {turnirs.map((turnir) => (
+                                        <SelectItem key={turnir.id} value={turnir.id.toString()}>
+                                            {turnir.name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <Select onValueChange={(value) => setFormData({...formData, categoryId: value})}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Выберите категорию"/>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {categories.map((category) => (
+                                        <SelectItem key={category.id} value={category.id.toString()}>
+                                            {category.name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <Select onValueChange={(value) => setFormData({...formData, playerId: value})}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Выберите игрока"/>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {players.map((player) => (
+                                        <SelectItem key={player.id} value={player.id.toString()}>
+                                            {player.name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <Select onValueChange={(value) => setFormData({...formData, color: value})}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Выберите цвет"/>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {Object.keys(ColorPlayer).map((color) => (
+                                        <SelectItem key={color} value={color}>
+                                            {ColorPlayer[color as keyof typeof ColorPlayer]}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <Select onValueChange={(value) => setFormData({...formData, city: value})}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Выберите город"/>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {Object.keys(cityTranslations).map((city) => (
+                                        <SelectItem key={city} value={city}>
+                                            {cityTranslations[city as keyof typeof cityTranslations]}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <Input
+                                type="number"
+                                name="gold"
+                                value={formData.gold}
+                                onChange={handleInputChange}
+                                placeholder="Gold"
+                            />
+                            <Input
+                                type="text"
+                                name="security"
+                                value={formData.security}
+                                onChange={handleInputChange}
+                                placeholder="Security"
+                            />
+                            Win/Lose<Input
+                            type="checkbox"
+                            name="win"
+                            checked={formData.win}
+                            onChange={handleInputChange}
+                        />
+                            <Input
+                                type="text"
+                                name="link"
+                                value={formData.link}
+                                onChange={handleInputChange}
+                                placeholder="Link"
+                            />
+                            <Button type="submit" className="h-7" disabled={!isFormValid}>
+                                Сохранить
+                            </Button>
+                            <Button className="h-7" onClick={handleCalculateStatistics} disabled={isPending}>
+                                {isPending ? 'Обновление...' : 'Обновить статистику'}
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            )}
         </div>
     );
 }
